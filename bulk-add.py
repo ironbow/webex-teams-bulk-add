6c162f820d4f54e315a1194bd29be28196c4b479
Ryan Wolfe <rn.wolfe@gmail.com>
2021-12-08 09:41:17 -0500

    Add option: add to provided rooms only, prompt user before continuing

--- bulk-add.py
+++ bulk-add.py
@@ -2,9 +2,18 @@
 from webexteamssdk import WebexTeamsAPI
 from webexteamssdk import exceptions
 import argparse
+from pprint import pprint
+import sys
 
 
-def add_users(token, team_name, users, add_to_all_rooms=False, are_moderators=False):
+def add_users(
+    token,
+    team_name,
+    users,
+    included_rooms=None,
+    add_to_all_rooms=False,
+    are_moderators=False,
+):
     # Init webexteamssdk
     api = WebexTeamsAPI(access_token=token)
 
@@ -12,16 +21,18 @@ def add_users(token, team_name, users, add_to_all_rooms=False, are_moderators=Fa
         me = api.people.me()
         print(f"> Connected to API as {me.displayName}")
     except exceptions.ApiError as e:
-        if e.status_code == 401: 
-            exit("Identity token unauthorized, please get a new Identity token from: https://developer.webex.com/docs/api/getting-started")
+        if e.status_code == 401:
+            exit(
+                "Identity token unauthorized, please get a new Identity token from: https://developer.webex.com/docs/api/getting-started"
+            )
         else:
             exit(e)
 
     # Load list of users to add
-    print(f"> Loaded {len(users)} total users to add..")
+    print(f"> Loaded {len(users)} total users..")
 
     # Find the team by the given team name
-    print(f"> Finding team details for {team_name}")
+    print(f"> Finding Team details for {team_name}")
     teams = api.teams.list()
     target_team = None
     for team in teams:
@@ -30,18 +41,46 @@ def add_users(token, team_name, users, add_to_all_rooms=False, are_moderators=Fa
 
     # If team name wasn't found.
     if not target_team:
-        exit(f"The team '{team_name}' was not found in the Teams for the given token.")
+        exit(f"The Team '{team_name}' was not found in the Teams for the given token.")
 
     # Add users to Team
     if are_moderators:
         print("> The '-m' flag was provided. ALL users will be added as moderators.")
 
-    # Try to get all the rooms in the Team
-    if add_to_all_rooms:
+    # Get the rooms to add the users to based on user-provided options
+    if included_rooms:
+        # User gave a list of rooms, find the rooms by name (as well as the default room)
+        rooms = [
+            room
+            for room in api.rooms.list(teamId=target_team.id)
+            if (room.title in included_rooms)
+        ]
+    elif add_to_all_rooms:
+        # User wants to add users to all rooms
         rooms = api.rooms.list(teamId=target_team.id)
+    else:
+        # No rooms were provided. Users will still join the default room upon joining the Team.
+        rooms = []
+
+    print(
+        f"> {len(users)} user(s) will be added the to the {target_team.name} Team in following rooms:",
+        *[room.title for room in rooms],
+        sep="\n\t-- ",
+    )
+    print(
+        "> Note: \
+        \n\tIf you want to add users to ALL rooms, use the '-a' flag. \
+        \n\tIf you want to add users to specific rooms, use the '-r' flag. \
+        \n\tUsers are always added to the Team's default room. This happens automatically upon joining the Team.\n"
+    )
+
+    # Confirm with user that things look right before continuing
+    if not input("Are you sure you want to continue? (y/N) ").lower() == "y":
+        print("Aborting..")
+        sys.exit(0)
 
     for email in members_to_add:
-        # Depending on how many users you're adding, the API may rate limit the script. 
+        # Depending on how many users you're adding, the API may rate limit the script.
         # The Teams SDK will handle that automatically, but you may see the script pause.
 
         # Add user to Team
@@ -55,23 +94,26 @@ def add_users(token, team_name, users, add_to_all_rooms=False, are_moderators=Fa
                 print(f"> {email} was already a member of the team. Skipping..")
             else:
                 print(e)
-        
+
         # Add user to rooms, if chosen
         for room in rooms:
             if room.isLocked:
-                # "Locked" rooms are any rooms that are "moderated" 
-                # such as the default Team room. Let's skip them 
+                # "Locked" rooms are any rooms that are "moderated"
+                # such as the default Team room. Let's skip them
                 # to reduce unnecessary errors.
                 continue
             try:
-                api.memberships.create(room.id, personEmail=email, isModerator=are_moderators)
+                api.memberships.create(
+                    room.id, personEmail=email, isModerator=are_moderators
+                )
             except exceptions.ApiError as e:
                 if e.response.status_code == 409:
-                    print(f"> {email} was already a member of room {room.title}. Skipping..")
+                    print(
+                        f"> {email} was already a member of room {room.title}. Skipping.."
+                    )
                 else:
                     print(e)
             print(f"> Adding {email} to room {room.title}..")
-
 
 
 if __name__ == "__main__":
@@ -101,9 +143,15 @@ if __name__ == "__main__":
         help="File that contains one user email per line.",
     )
     parser.add_argument(
+        "-r",
+        "--rooms",
+        help="Specify room names that given users should be added to. Should be a quote-wrapped, semicolon-separated list of room names.",
+        default=None,
+    )
+    parser.add_argument(
         "-a",
         "--all-rooms",
-        help="Add the users to all the existing Team rooms.",
+        help="Add the users to all the existing Team rooms. Ignored if '-r' flag is used.",
         default=False,
         action="store_true",
     )
@@ -122,4 +170,15 @@ if __name__ == "__main__":
     except FileNotFoundError:
         exit(f"The file '{args.file}' does not exist. Please double check the path.")
 
-    add_users(args.identity_token, args.team, members_to_add, args.all_rooms, args.moderator)
+    if args.rooms:
+        # Split the rooms by semicolons and remove surrounding spaces
+        args.rooms = [room.strip() for room in args.rooms.split(";")]
+
+    add_users(
+        args.identity_token,
+        args.team,
+        members_to_add,
+        args.rooms,
+        args.all_rooms,
+        args.moderator,
+    )
